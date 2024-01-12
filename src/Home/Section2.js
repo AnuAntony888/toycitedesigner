5d3ed1e055e1d54977369d9d46bfa0e5e5513096
websoullabsanu <websoullabsanu1@gmail.com>
2024-01-12 12:08:44 +0530

    add parallex

--- src/Home/Section2.js
+++ src/Home/Section2.js
@@ -5,8 +5,8 @@ import img1 from "../Assets/second.jpg";
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import "aos/dist/aos.css";
-import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
-
+import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
+import { Parallax, Background } from "react-parallax";
 const Section2 = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
@@ -60,13 +60,17 @@ const Section2 = () => {
             }}
           ></Box>
 
-          <Grid container Spacing={3} sx={{ zIndex: 1, p: "12%" ,textAlign:'justify'}}>
+          <Grid
+            container
+            Spacing={3}
+            sx={{ zIndex: 1, p: "12%", textAlign: "justify" }}
+          >
             <Grid item lg={6} md={6} sm={12} xs={12}>
               <TypographyText
                 Typography={<>Pre-School</>}
                 color="#fff"
                 fontWeight="600"
-                textAlign={!isSmallScreen ? "left" :"centre"}
+                textAlign={!isSmallScreen ? "left" : "centre"}
                 variant={!isSmallScreen ? "h2" : "h4"}
               />
               <br />
@@ -83,16 +87,14 @@ const Section2 = () => {
                 color="#fff"
                 textAlign="left"
               />
-                <br />
-              <ArrowCircleRightIcon sx={{color:'white',  
-              fontSize:'3rem',
-            }}/>
+              <br />
+              <ArrowCircleRightIcon sx={{ color: "white", fontSize: "3rem" }} />
             </Grid>
             <Grid item lg={6} md={6} sm={12} xs={12} sx={{ margin: "auto" }}>
               <Grid container Spacing={2}>
                 {imagelogo.map((data, index) => (
-                  <Grid item xs={3}  key={index}>
-                    <img src={data.img} alt="" width={'100%'}/>
+                  <Grid item xs={3} key={index}>
+                    <img src={data.img} alt="" width={"100%"} />
                   </Grid>
                 ))}
               </Grid>
@@ -102,6 +104,93 @@ const Section2 = () => {
       </Grid>
 
       {/************************************************* */}
+      <Parallax
+        bgImage="https://cdn.firstcry.com/education/2022/11/06094158/Toy-Names-For-Kids.jpg"
+        bgImageAlt="the cat"
+        strength={200}
+        style={{ zIndex: -1 }}
+      >
+        <Grid
+          container
+          spacing={2}
+          className="large-header"
+          sx={{ height: !isSmallScreen ? "105vh" : "auto" }}
+        >
+          <Grid
+            item
+            lg={12}
+            md={12}
+            sm={12}
+            xs={12}
+            // sx={{
+            //   backgroundImage: `url(${img1})`,
+            //   backgroundSize: "cover",
+            //   objectFit: "cover",
+            //   height: "100%",
+            //   display: "flex",
+            //   alignItems: "center",
+
+            //   backgroundRepeat: "no-repeat",
+            //   backgroundPosition: "bottom",
+            // }}
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                opacity: ".4",
+                 backgroundColor: "#000000", // Adjust color and opacity as needed
+              }}
+            ></Box>
+
+            <Grid
+              container
+              Spacing={3}
+              sx={{ zIndex: 1, p: "12%", textAlign: "justify" }}
+            >
+              <Grid item lg={6} md={6} sm={12} xs={12}>
+                <TypographyText
+                  Typography={<>Pre-School</>}
+                  color="#fff"
+                  fontWeight="600"
+                  textAlign={!isSmallScreen ? "left" : "centre"}
+                  variant={!isSmallScreen ? "h2" : "h4"}
+                />
+                <br />
+                <TypographyText
+                  Typography={
+                    <>
+                      Ignite young minds with our captivating preschool toys
+                      collection! Specially designed for early learners, these
+                      toys blend fun and education seamlessly. Explore the joy
+                      of learning with our thoughtfully curated selection for
+                      the littlest adventurers.
+                    </>
+                  }
+                  color="#fff"
+                  textAlign="left"
+                />
+                <br />
+                <ArrowCircleRightIcon
+                  sx={{ color: "white", fontSize: "3rem" }}
+                />
+              </Grid>
+              <Grid item lg={6} md={6} sm={12} xs={12} sx={{ margin: "auto" }}>
+                <Grid container Spacing={2}>
+                  {imagelogo.map((data, index) => (
+                    <Grid item xs={3} key={index}>
+                      <img src={data.img} alt="" width={"100%"} />
+                    </Grid>
+                  ))}
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Parallax>
     </>
   );
 };
@@ -121,6 +210,11 @@ const imagelogo = [
   {
     img: "https://static.wixstatic.com/media/3fff3e_0c4400a66a0942468a4065d983681535~mv2.png/v1/fill/w_296,h_168,q_90/3fff3e_0c4400a66a0942468a4065d983681535~mv2.webp",
   },
-  {img:'https://static.wixstatic.com/media/3fff3e_630517c506824605a7f1c658f0f5d39f~mv2.png/v1/fill/w_296,h_168,q_90/3fff3e_630517c506824605a7f1c658f0f5d39f~mv2.webp'}
+  {
+    img: "https://static.wixstatic.com/media/3fff3e_630517c506824605a7f1c658f0f5d39f~mv2.png/v1/fill/w_296,h_168,q_90/3fff3e_630517c506824605a7f1c658f0f5d39f~mv2.webp",
+  },
 
-,{img:'https://static.wixstatic.com/media/3fff3e_96c8224664824e45a2dbf643180a7c1d~mv2.png/v1/fill/w_296,h_168,q_90/3fff3e_96c8224664824e45a2dbf643180a7c1d~mv2.webp'}];
+  {
+    img: "https://static.wixstatic.com/media/3fff3e_96c8224664824e45a2dbf643180a7c1d~mv2.png/v1/fill/w_296,h_168,q_90/3fff3e_96c8224664824e45a2dbf643180a7c1d~mv2.webp",
+  },
+];
